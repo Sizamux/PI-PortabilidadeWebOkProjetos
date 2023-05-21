673f190aa0a55977ab45128834c941467030108d
Your Name <sizamux@hotmail.com>
2023-05-21 17:49:02 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PI_PorrtabilidadeWebOkPrrojetos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
